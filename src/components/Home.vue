1cb0ca4326f27dac230a06be8b7896aaee256def
CLPJ <1803686933@qq.com>
2020-04-01 15:47:18 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <el-button type="info" @click="logout">退出</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+
+//   data () {
+//     return {
+//       methods: {
+//         logout () {
+//           window.sessionStorage.clear()
+//           this.$router.push('/login')
+//         }
+//       }
+//     }
+//   }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>

15609339492a5c7b639710c75c95a9638e081e2b
CLPJ <1803686933@qq.com>
2020-04-03 17:11:28 +0800

    完成用户列表功能的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,95 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/logo1.png" alt="">
+        <span>电商后台管理</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <!-- isCollapse 为true 代表被折叠 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+         <!-- 侧边栏菜单区域 -->
+         <el-menu background-color="#333744" text-color="#fff"
+         active-text-color="#409EFF" :unique-opened="true" :collapse="isCollapse"
+         :collapse-transition="false" :router="true" :default-active="activePath">
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id  + ''" v-for="item in menulist" :key="item.id">
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/' + subItem.path)" >
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+         </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      // 左侧菜单栏的数据
+      menulist: [],
+      iconsObj: {
+        125: 'iconfont icon-user',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao'
+      },
+      // 是否折叠
+      isCollapse: false,
+      // 保存激活的链接地址
+      activePath: ''
+    }
+  },
+  created () {
+    this.getMenuList()
+    this.avtivePath = window.sessionStorage.getItem('activePath')
+  },
   methods: {
     logout () {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    // 获取所有的菜单
+    async getMenuList () {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+    },
+    // 点击按钮切换折叠与展开
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
+    },
+    // 保存连接的激活状态
+    saveNavState (activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
     }
   }
 
@@ -27,5 +107,48 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
 
+  }
+  img {
+    width: 55px;
+  }
+}
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: 0;
+  }
+}
+.el-main {
+  background-color: #E9EEF3;
+}
+.iconfont {
+  margin-right: 10px;
+}
+.toggle-button {
+  background-color: #4A5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
 </style>
